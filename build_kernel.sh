4cc2d21c569b8ca42bf2f58128ee201d7573f7f3
Thomas Turner <thomas_turner@talktalk.net>
2024-12-02 16:38:56 +0000

    a21s: Build with LLVM enabled

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,6 +5,7 @@ export PATH=/home/thomas/toolchains/aarch64-linux-android-4.9/bin/:/home/thomas/
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 export ARCH=arm64
+export LLVM=1
 
 mkdir out
 make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-androidkernel- CC=clang CLANG_TRIPLE=aarch64-linux-gnu- exynos850-a21snsxx_defconfig
